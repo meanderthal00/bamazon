e5ac15443baf48a2d0e01103115ba3607824a47c
Meanderthal00 <meanderthal00@yahoo.com>
2018-01-23 07:19:44 -0500

    working on getting the inquirer working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,69 +13,81 @@ var connection = mysql.createConnection({
 
 // checking connection to the database
 connection.connect(function (err) {
-    if (err) throw err;
+    if (err) throw (err);
     console.log("Connected as " + connection.threadId);
+    // starting function to display items
+    purchase();
 });
 
-// starting function to display items
-purchase();
+
+var stockArray = [];
+console.log("stockArrayI" + stockArray);
 
 function purchase() {
-    connection.query('SELECT * FROM inventory', function (err, res) {
-            // console.log(res);s
+    connection.query('SELECT * FROM inventory', (err, res) => {
+        // console.log(res);
+        if (err) throw err;
+        console.log("Here is a list of avaliable items:");
+        console.log('=============================================================================================================');
+        // for (var i = 0; i < res.length; i++) {
+        //     stockArray.push(res[i].item)
+        // }
+        // console.log(stockArray);
+    // })
+    // using a loop to display products/prices to user
+    for (var i = 0; i < res.length; i++) {
+        console.log('ID: ' + res[i].id + ' | Product: ' + res[i].product_name + ' | Department: ' + res[i].department_name + ' | Price: ' + res[i].price + ' | Remaining: ' + res[i].stock_quantity);
 
-            // using a loop to display products/prices to user
-            for (var i = 0; i < res.length; i++) {
-                console.log('ID: ' + res[i].id + ' | Product: ' + res[i].product_name + ' | Department: ' + res[i].department_name + ' | Price: ' + res[i].price + ' | Remaining: ' + res[i].stock_quantity);
-
-                console.log('=============================================================================================================');
-            }
-
-            // setting up inquirer
-            // first prompt asks for the item number of what they want to purchase
-
-            inquirer
-                .prompt([{
-                    // prompt for purchase item
-                        type: 'list',
-                        name: 'stock',
-                        message: 'Please choose the ID number of desired item ',
-                        choices: function (value) {
-                            var stockArray = [];
-                            for (var i = 0; i < res.length; i++) {
-                                stockArray.push(res[i].id);
-                            }
-                            return stockArray;
-                        }
-                    }, {
-                        // prompt for quantity
-                        name: 'quantity',
-                        type: 'input',
-                        message: 'Please enter desired quantity.',
-                        // validate: function (value) {
-                        //     if (isNaN(value) == false) {
-                        //         return true;
-                        //     } else {
-                        //         return false;
-                        //     }
-                        // }
-                    }]).then (function(answer){
-                        for (var i = 0; i < res.length; i ++){
-                            if (res[i].id == answer.list) {
-                                var itemChoice = res[i];
-                            }
-                        }
-                        console.log(itemChoice);
-
-                })
-               
-
-
-
-
-
-
+        console.log('=============================================================================================================');
+        // console.log("stockArrayII" + stockArray);
+    }
     })
+    // setting up inquirer
+    // first prompt asks for the item number of what they want to purchase
+
+    inquirer
+        .prompt([{
+            // prompt for purchase item
+            type: 'rawlist',
+            name: 'stock',
+            message: 'Please choose the ID number of desired item ',
+            choices: [function (value) {
+                // var stockArray = [];
+                for (var i = 0; i < res.length; i++) {
+                    stockArray.push(res[i].inventory);
+                }
+                console.log(stockArray);
+                return stockArray;
+            }]
+        }, {
+            // prompt for quantity
+            name: 'quantity',
+            type: 'input',
+            message: 'Please enter desired quantity.',
+            validate: function (value) {
+                if (isNaN(value) == false) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }]).then(function (answer) {
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].id == answer.list) {
+                    var itemChoice = res[i];
+                }
+            }
+            console.log(itemChoice);
+
+        })
+
+
+
+
+
+
+
+
 }
 
 
